e46dbc62998b785d5caece944df8aeadbd05b34b
eduar2tc@gmail.com <eduar2tc@gmail.com>
2022-06-01 04:10:18 -0500

    first commit

---
+++ data-types.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  //Strings
+  String name = "Eduardo";
+  String lastName = "TC";
+  print('Hello guys my name is ${name} ' + lastName);
+
+  //Integers
+  int age = 100;
+  int moneyInBank = -50;
+  print('Hello guys my name is ${name} ' +
+      lastName +
+      ', ${age} years old \n'
+          ' im have ${moneyInBank} money in the bank');
+}
